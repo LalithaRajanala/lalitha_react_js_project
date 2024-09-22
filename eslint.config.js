88677ff921145b572cdd27f647de8f59ff341510
Lalitha Rajanala <lalitha@gmail.com>
2024-09-22 15:16:00 -0700

    Update eslint config with flag config props for env variable

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,25 @@
 // eslint.config.js
 import js from "@eslint/js";
+import globals from "globals";
 
-export default [js.configs.recommended];
+export default [
+  js.configs.recommended,
+  {
+    files: ["src/**/*.js", "src/**/*.jsx"],
+    rules: {
+      semi: "error",
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        env: true,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+];
