8ffc833c5430d18fe07d5755cb38be65a859ebf0
Lalitha Rajanala <lalitha@gmail.com>
2024-09-22 15:48:08 -0700

    Use JSX to render top level app and use Pet component via JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,19 @@ import React from "react";
 import { createRoot }  from "react-dom";
 import Pet from './Pet';
 
-
 // Your script goes here
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Mochi",
-      animal: "Dog",
-      breed: "Maltipoo"
-    }),
-    React.createElement(Pet, {
-      name: "Butter",
-      animal: "Dog",
-      breed: "Maltipoo"
-    }),
-    React.createElement(Pet, {
-      name: "Waffles",
-      animal: "Dog",
-      breed: "Maltipoo"
-    }),
-    React.createElement(Pet, {
-      name: "Snickers",
-      animal: "Dog",
-      breed: "Maltipoo"
-    }),
-  ]);
+  return (
+    <div>
+      <h1>"Adopt me!"</h1>
+      <Pet name="Mochi" animal="Dog" breed="Maltipoo" />
+      <Pet name="Butter" animal="Dog" breed="Maltipoo" />
+      <Pet name="Waffles" animal="Dog" breed="Maltipoo" />
+      <Pet name="Snickers" animal="Dog" breed="Maltipoo" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
