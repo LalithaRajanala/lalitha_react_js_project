646a9e4cd618e732bc110fc5bd2f24a40483f31a
Lalitha Rajanala <lalitha@gmail.com>
2024-10-20 23:12:47 -0700

    Use reactquery to make api call

--- src/fetchPet.js
+++ src/fetchPet.js
@@ -1,6 +1,5 @@
 // async function to make api call to fetch pets at an id
-const fetchPet = async ({ query }) => {
-  const id = query[0];
+const fetchPet = async (id) => {
   const result = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
 
   if (!result.ok) {
